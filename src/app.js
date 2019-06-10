c07c984ad89fa05feb21a9b5e0807b27f79adc57
tackyunicorn <joshjoseph97@gmail.com>
2019-06-10 07:35:33 +0530

    :sparkles: added ejs view engine and routes for legals

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+const viewsPath = path.join(__dirname, '../templates/views')
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// set up view engine
+app.set('view engine', 'ejs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
 // create home route
 app.get('/', (req, res) => {
-    res.send('Welcome to kizuna!')
+    res.render('feed')
+})
+
+// create terms-conditions route
+app.get('/terms-conditions', (req, res) => {
+    res.render('terms-conditions')
+})
+
+// create privacy-policy route
+app.get('/privacy-policy', (req, res) => {
+    res.render('privacy-policy')
 })
 
 // start the server
