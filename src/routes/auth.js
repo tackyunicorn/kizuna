090d1a68650bbdfc2053a0343f5c9e15857c041a
tackyunicorn <joshjoseph97@gmail.com>
2019-06-13 18:41:28 +0530

    :children_crossing: added auth and profile routes

---
+++ src/routes/auth.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const passport = require('passport')
+
+require('../passport/passport-setup')
+
+// auth login
+router.get('/login', (req, res) => {
+    if (req.user) {
+        res.redirect('/profile/feed')
+    } else {
+        res.render('login', { user: req.user })
+    }
+})
+
+// auth logout
+router.get('/logout', (req, res) => {
+    // handle with passport
+    req.logout()
+    res.redirect('/')
+})
+
+// auth with google
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile']
+}))
+
+// callback route for google to redirect to
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.redirect('/profile/feed')
+})
+
+module.exports = router
