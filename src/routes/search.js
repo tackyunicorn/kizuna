bada2b5320a6f9d88f4c54a345e3bba914f0e1be
tackyunicorn <joshjoseph97@gmail.com>
2019-06-14 16:08:19 +0530

    :children_crossing: full text search endpoint added

---
+++ src/routes/search.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+
+const User = require('../models/user')
+
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+        // if user is not logged in
+        res.redirect('/auth/login')
+    } else {
+        // if logged in
+        next()
+    }
+}
+
+router.post('/', authCheck, async (req, res) => {
+    try {
+        const dbResponse = await User.find({
+            $text: {
+                $search: req.body.searchTerm
+            }
+        }, {
+            score: {
+                $meta: "textScore"
+            }
+        }).sort({
+            score: {
+                $meta: 'textScore'
+            }
+        }).limit(6)
+        const results = dbResponse.filter((user) => user.id !== req.user.id)
+        res.send({ results })
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+module.exports = router
