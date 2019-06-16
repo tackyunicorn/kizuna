b3ef4ab49b3bb0ac717abb66cce1c91e10103277
tackyunicorn <joshjoseph97@gmail.com>
2019-06-16 19:12:14 +0530

    :children_crossing: sending sorted post data to my-profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
+const Post = require('../models/post')
+
 const authCheck = (req, res, next) => {
     if (!req.user) {
         // if user is not logged in
@@ -10,15 +12,27 @@ const authCheck = (req, res, next) => {
     }
 }
 
-router.get('/', authCheck, (req, res) => {
-    res.render('profile', {
-        user: req.user,
-        self: true
-    })
+router.get('/', authCheck, async (req, res) => {
+    try {
+        const posts = await Post.find({
+            userid: req.user.id
+        }).sort({
+            timestamp: -1
+        }).limit(10)
+        res.render('profile', {
+            user: req.user,
+            self: true,
+            posts
+        })
+    } catch (e) {
+        console.error(e)
+    }
 })
 
 router.get('/feed', authCheck, (req, res) => {
-    res.render('feed', { user: req.user })
+    res.render('feed', {
+        user: req.user
+    })
 })
 
 module.exports = router
