68198ad50eb39e5fbe040486d0c378b9dee9b539
tackyunicorn <joshjoseph97@gmail.com>
2019-06-16 19:19:22 +0530

    :children_crossing: sending public post data to public profile

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const User = require('../models/user')
+const Post = require('../models/post')
 
 const authCheck = (req, res, next) => {
     if (!req.user) {
@@ -13,12 +14,22 @@ const authCheck = (req, res, next) => {
 }
 
 router.get('/:id', authCheck, async (req, res) => {
+    if (req.user.id == req.params.id) {
+        return res.redirect('/profile/')
+    }
     try {
         const foundUser = await User.find({ _id: req.params.id })
+        const posts = await Post.find({
+            userid: req.params.id,
+            private: false
+        }).sort({
+            timestamp: -1
+        }).limit(10)
         res.render('profile', {
             user: req.user,
             foundUser: foundUser[0],
-            self: false
+            self: false,
+            posts
         })
     } catch (e) {
         console.error(e)
