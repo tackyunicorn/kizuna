6141324154368ca0af58847a33aa21526aeb0ddd
tackyunicorn <joshjoseph97@gmail.com>
2019-06-18 22:53:16 +0530

    :children_crossing: adding the aggregation pipeline for the feed route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const Post = require('../models/post')
+const Follow = require('../models/follow')
 
 const authCheck = (req, res, next) => {
     if (!req.user) {
@@ -29,10 +30,113 @@ router.get('/', authCheck, async (req, res) => {
     }
 })
 
-router.get('/feed', authCheck, (req, res) => {
-    res.render('feed', {
-        user: req.user
-    })
+router.get('/feed', authCheck, async (req, res) => {
+    try {
+        const feed = await Follow.aggregate([{
+            $match: {
+                userid: req.user.id
+            }
+        }, {
+            $project: {
+                _id: 0,
+                follows: 1,
+                timestamp: 1,
+                follows: {
+                    $toObjectId: '$follows'
+                }
+            }
+        }, {
+            $lookup: {
+                from: 'users',
+                localField: 'follows',
+                foreignField: '_id',
+                as: 'userinfo'
+            }
+        }, {
+            $project: {
+                follows: 1,
+                timestamp: 1,
+                userinfo: {
+                    $arrayElemAt: [
+                        '$userinfo', 0
+                    ]
+                },
+                follows: {
+                    $toString: '$follows'
+                }
+            }
+        }, {
+            $lookup: {
+                from: 'posts',
+                localField: 'follows',
+                foreignField: 'userid',
+                as: 'posts'
+            }
+        }, {
+            $unwind: {
+                path: '$posts',
+                preserveNullAndEmptyArrays: true
+            }
+        }, {
+            $project: {
+                userinfo: 1,
+                timestamp: 1,
+                posts: {
+                    $ifNull: [
+                        "$posts", {
+                            private: false
+                        }
+                    ]
+                }
+            }
+        }, {
+            $match: {
+                'posts.private': false
+            }
+        }, {
+            $sort: {
+                'posts.timestamp': -1
+            }
+        }, {
+            $group: {
+                _id: '$userinfo._id',
+                timestamp: {
+                    $push: '$timestamp'
+                },
+                userinfo: {
+                    $push: '$userinfo'
+                },
+                posts: {
+                    $push: '$posts'
+                }
+            }
+        }, {
+            $project: {
+                _id: 0,
+                userinfo: {
+                    $arrayElemAt: [
+                        '$userinfo', 0
+                    ]
+                },
+                timestamp: {
+                    $arrayElemAt: [
+                        '$timestamp', 0
+                    ]
+                },
+                posts: 1
+            }
+        }, {
+            $sort: {
+                'timestamp': -1
+            }
+        }])
+        res.render('feed', {
+            user: req.user,
+            feed
+        })
+    } catch (e) {
+        console.error(e)
+    }
 })
 
 module.exports = router
