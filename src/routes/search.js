63c2703ea1631b53586babeaa64fca926f5c56e2
tackyunicorn <joshjoseph97@gmail.com>
2019-06-20 11:09:49 +0530

    :children_crossing: reducing search result limit

--- src/routes/search.js
+++ src/routes/search.js
@@ -26,7 +26,7 @@ router.post('/', authCheck, async (req, res) => {
             score: {
                 $meta: 'textScore'
             }
-        }).limit(10)
+        }).limit(7)
         const results = dbResponse.filter((user) => user.id !== req.user.id)
         res.send({ results })
     } catch (e) {
